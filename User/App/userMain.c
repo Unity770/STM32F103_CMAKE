1bb3dbeb625561e374fa4e7e1a635ad262969459
DESKTOP-1N8T5IS\Fy770 <1969751339@qq.com>
2025-12-05 12:11:24 +0800

    1.iic读6050 2.笔记

--- User/App/userMain.c
+++ User/App/userMain.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include "elog.h"
 #include "gpio.h"
+#include "i2c.h"
+#include "MPU6050_Reg.h"
 
 //UART1相关定义
 uint8_t uart1_rx_buffer[256];
@@ -15,6 +17,9 @@ fy_uart_t uart1;
 
 //外部中断相关定义(旋转编码器)
 uint16_t enCoder_count = 0;
+
+//定时器2相关定义
+uint16_t timer2_overflow_count = 0;
 
 void uart1_init(void)
 {
@@ -28,7 +33,6 @@ void easy_logger_out(const char *log, size_t size)
 {
     uart1.uartTx(&uart1, (const uint8_t *)log, size);
 }
-
 
 void easy_logger_init(void)
 {
@@ -56,6 +60,8 @@ void easy_logger_init(void)
     elog_start();
 }
 
+
+//外部中断回调函数
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
   /* Prevent unused argument(s) compilation warning */
@@ -86,21 +92,62 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     }
     
 }
+
+//iic测试
+int16_t AccX,AccY,AccZ,GyroX,GyroY,GyroZ;
+uint8_t mpu_id;
+uint8_t mpu6050_addr = MPU6050_ADDRESS;
+void init_mpu6050()
+{
+    uint8_t tmp_reg_vlaue;
+    tmp_reg_vlaue = 0x01;
+    HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDRESS, MPU6050_PWR_MGMT_1,   I2C_MEMADD_SIZE_8BIT, &tmp_reg_vlaue, 1, 1000);
+    tmp_reg_vlaue = 0x00;  
+    HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDRESS, MPU6050_PWR_MGMT_2,   I2C_MEMADD_SIZE_8BIT, &tmp_reg_vlaue, 1, 1000);
+    tmp_reg_vlaue = 0x09;  
+    HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDRESS, MPU6050_SMPLRT_DIV,   I2C_MEMADD_SIZE_8BIT, &tmp_reg_vlaue, 1, 1000);
+    tmp_reg_vlaue = 0x06;  
+    HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDRESS, MPU6050_CONFIG,       I2C_MEMADD_SIZE_8BIT, &tmp_reg_vlaue, 1, 1000);
+    tmp_reg_vlaue = 0x18;
+    HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDRESS, MPU6050_GYRO_CONFIG,  I2C_MEMADD_SIZE_8BIT, &tmp_reg_vlaue, 1, 1000);
+    tmp_reg_vlaue = 0x18;
+    HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDRESS, MPU6050_ACCEL_CONFIG, I2C_MEMADD_SIZE_8BIT, &tmp_reg_vlaue, 1, 1000);
+}
+void test_iic_mpu6050()
+{
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_WHO_AM_I, I2C_MEMADD_SIZE_8BIT, &mpu_id, 1, 1000);
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_ACCEL_XOUT_H, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&AccX, 2, 1000);
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_ACCEL_YOUT_H, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&AccY, 2, 1000);
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_ACCEL_ZOUT_H, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&AccZ, 2, 1000);
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_GYRO_XOUT_H, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&GyroX, 2, 1000);
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_GYRO_YOUT_H, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&GyroY, 2, 1000);
+    HAL_I2C_Mem_Read(&hi2c2, MPU6050_ADDRESS, MPU6050_GYRO_ZOUT_H, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&GyroZ, 2, 1000);
+    //高低字节交换
+    AccX = (AccX >> 8) | (AccX << 8);
+    AccY = (AccY >> 8) | (AccY << 8);
+    AccZ = (AccZ >> 8) | (AccZ << 8);
+    GyroX = (GyroX >> 8) | (GyroX << 8);
+    GyroY = (GyroY >> 8) | (GyroY << 8);
+    GyroZ = (GyroZ >> 8) | (GyroZ << 8);
+}
+
 void user_main(void)
 {
     /* Example usage of fy_uart and ring buffer can be placed here */
     uint32_t start_tick = HAL_GetTick();
+
+    //日志
     uart1_init();
     easy_logger_init();
+    init_mpu6050();
     while (1)
     {
         HAL_GetTick();
-        if (HAL_GetTick() - start_tick >= 10)
+        if (HAL_GetTick() - start_tick >= 500)
         {
+            test_iic_mpu6050();
+            elog_a("MPU6050","MPU6050 ID: 0x%02X,AccX:%d, AccY:%d, AccZ:%d,GyroX:%d,GyroY:%d,GyroZ:%d", mpu_id, AccX, AccY, AccZ, GyroX, GyroY, GyroZ);
             start_tick = HAL_GetTick();
-            // 每隔1秒发送一次数据
-            // const char *msg = "Hello from UART1!";
-            elog_a("enCoder", "count:[%d]", enCoder_count);
         }
     }
     
